3f903e37f99155450c212743bce5d07a144b6b9d
stefangohlke <gohlke.stefan@yahoo.de>
2023-01-29 00:41:56 +0100

    Switch to base deps. Add CI. Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,46 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 // import { Server } from 'socket.io'
-import express from 'express'
-import http from 'node:http'
-import {GraphQLServer, JsonLogger} from '@dreamit/graphql-server'
-import { 
-    userSchema, 
-    userSchemaResolvers 
-} from './ExampleSchemas'
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express, {Response} from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express()
-app.use(cors());
-app.use(bodyParser.text());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-const graphqlServer = new GraphQLServer(
-    {
-        schema: userSchema,
-        rootValue: userSchemaResolvers,
-        logger: new JsonLogger('sse-server', 'user-service')
-    }
-)
+app.use(cors())
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 const messages: Array<string> = ['first message']
-let clients: Array<any> = []
+let clients: Array<{
+    id: number,
+    response:Response
+}> = []
 
 app.get('/messages', (request, response) => {
     const headers = {
         'Content-Type': 'text/event-stream',
         'Connection': 'keep-alive',
         'Cache-Control': 'no-cache'
-      };
-      response.writeHead(200, headers);
+    }
+    response.writeHead(200, headers)
     
-      const data = `data: ${JSON.stringify(messages)}\n\n`;
+    const data = `data: ${JSON.stringify(messages)}\n\n`
     
-      response.write(data);
+    response.write(data)
     
-      const clientId = Date.now();
+    const clientId = Date.now()
     
-      const newClient = {
+    const newClient = {
         id: clientId,
         response
-      };
+    }
     
-      clients.push(newClient);
+    clients.push(newClient)
     
-      request.on('close', () => {
-        console.log(`${clientId} Connection closed`);
-        clients = clients.filter(client => client.id !== clientId);
-      });
+    request.on('close', () => {
+        console.log(`${clientId} Connection closed`)
+        clients = clients.filter(client => client.id !== clientId)
+    })
 })
 
 app.post('/submitMessage', (request, response) => {
@@ -68,20 +58,20 @@ app.listen(3000, () => {
     console.log('listening on localhost:3000')
 })
 
-function sendEventsToAll(newMessage: string) {
-    clients.forEach(client => client.response.write(`data: ${JSON.stringify(newMessage)}\n\n`))
+function sendEventsToAll(newMessage: string): void {
+    for (const client of clients) client.response.write(`data: ${JSON.stringify(newMessage)}\n\n`)
 }
 
-function addNewMessage(newMessage: string) {
-    messages.push(newMessage);
-    sendEventsToAll(newMessage);
+function addNewMessage(newMessage: string): void {
+    messages.push(newMessage)
+    sendEventsToAll(newMessage)
 }
 
-setInterval(function () { 
+setInterval(function() { 
     addNewMessage(`New Message on ${new Date()}`)
-}, 5000);
+}, 5000)
 
-setInterval(function () { 
+setInterval(function() { 
     // Send all messages to all clients
-    clients.forEach(client => client.response.write(`data: ${JSON.stringify(messages)}\n\n`))
-}, 20000);
+    for (const client of clients) client.response.write(`data: ${JSON.stringify(messages)}\n\n`)
+}, 20_000)
